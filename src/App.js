ff17dde4d5df116d686cfbf07d6ee7ad98380f0c
alimtegar <alimtegar4@gmail.com>
2019-02-26 21:32:24 +0700

    #1 - Version 1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,116 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import Libraries
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import "@tensorflow/tfjs";
+
 class App extends Component {
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+  componentDidMount() {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      const webCamPromise = navigator.mediaDevices
+        .getUserMedia({
+          audio: false,
+          video: {
+            facingMode: "user"
+          }
+        })
+        .then((stream) => {
+          window.stream = stream;
+
+          this.videoRef.current.srcObject = stream;
+
+          return new Promise((resolve, reject) => {
+            this.videoRef.current.onloadedmetadata = () => {
+              resolve();
+            };
+          });
+        });
+
+      const modelPromise = cocoSsd.load();
+
+      Promise.all([modelPromise, webCamPromise])
+        .then((values) => {
+          this.detectFrame(this.videoRef.current, values[0]);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }
+
+  detectFrame = (video, model) => {
+    model.detect(video)
+      .then((predictions) => {
+        this.renderPredictions(predictions);
+
+        requestAnimationFrame(() => {
+          this.detectFrame(video, model);
+        });
+      });
+  }
+
+  renderPredictions = (predictions) => {
+    const ctx = this.canvasRef.current.getContext('2d');
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Font options
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+      const width = prediction.bbox[2];
+      const height = prediction.bbox[3];
+
+      // Draw the bounding box
+      ctx.strokeStyle = "#00FFFF";
+      ctx.lineWidth = 4;
+      ctx.strokeRect(x, y, width, height);
+
+      // Draw the label background
+      ctx.fillStyle = "#00FFFF";
+
+      const textWidth = ctx.measureText(prediction.class).width;
+      const textHeight = parseInt(font, 10);
+
+      ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+    });
+
+    predictions.forEach((prediction) => {
+      const x = prediction.bbox[0];
+      const y = prediction.bbox[1];
+
+      // Draw the text last to ensure it's on top
+      ctx.fillStyle = "#000000";
+      ctx.fillText(prediction.class, x, y);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <video 
+          autoPlay
+          playsInline
+          muted
+          ref={this.videoRef}
+          width="360"
+          height="270"
+          className="fixed"
+        />
+        <canvas 
+          ref={this.canvasRef}
+          width="360"
+          height="270"
+          className="fixed"
+        />
       </div>
     );
   }
